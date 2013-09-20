772138e7a2fddfd84e998a25aa64aad69bf812f6
Casey Battaglino <cjbattagl@gmail.com>
2013-09-20 16:16:47 -0400

    edit

--- mfiles/judge1dpart.m
+++ mfiles/judge1dpart.m
@@ -1,6 +1,6 @@
 % Report metrics on 1d partition quality into p equal parts
 % future: allow bins to represent partitions
-function [] = judge1dpart(A,p)
+function [balance cut] = judge1dpart(A,p)
   n = size(A,1);
   m = nnz(A);
   loads = zeros(p,1);
@@ -8,6 +8,6 @@ function [] = judge1dpart(A,p)
   for i=1:p
     loads(i) = sum(sum(A((i-1)*floor(n/p)+1:i*floor(n/p),:)));
   end
-  imbalance = max(loads)/min(loads)
-  percentedgescut = cutsize(A,p)/m;
+  balance = max(loads)/min(loads)
+  cut = cutsize(A,p)/m;
 end
