ef7fd01c0a9e422e4f8d90bf5235e066b02c0019
Casey John Battaglino <cbattaglino3@ion.cc.gatech.edu>
2013-10-09 19:30:03 -0400

    fixed ER gen

--- mfiles/erdos_generator.m
+++ mfiles/erdos_generator.m
@@ -2,23 +2,13 @@
 % is set to true, return directed graph instead
 % Existing ER generators for MATLAB aren't scalable for massive graphs because
 % they create a dense random matrix.
-% This implementation scales by initializing one row at a time.
 function [A] = erdos_generator(N, p, isDirected, seed)
   if (nargin == 4)
     rand('seed',seed);
   end
 
-  A = sparse(N,N);
+  A = spones(sprand(N,N,p)); 
   
-  for i=1:N
-    rand('seed',seed+i);
-    vec = rand(N,1);
-    vec = vec < p;
-    vec = sparse(vec);
-    A(:,i) = vec;
-  end
-
-  A = A(~eye(N)); %remove self-edges
   if (nargin == 3)
     if (~isDirected)
       T = tril(A);
@@ -28,4 +18,8 @@ function [A] = erdos_generator(N, p, isDirected, seed)
     T = tril(A);
     A = T + T';
   end
+
+  %remove diagonals
+  [nRows,nCols] = size(A);
+  A(1:(nRows+1):nRows*nCols) = 0;
 end
