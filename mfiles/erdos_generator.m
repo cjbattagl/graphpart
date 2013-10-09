27ba0b49a6188d322264ae851e15cba26d86c680
Casey John Battaglino <cbattaglino3@ion.cc.gatech.edu>
2013-10-09 19:13:44 -0400

    scalable ER gen?

--- mfiles/erdos_generator.m
+++ mfiles/erdos_generator.m
@@ -1,14 +1,30 @@
 % Return A, adj. matrix for undirected E-R(N,p). If isDirected argument
 % is set to true, return directed graph instead
-function [A] = erdos_generator(N, p, isDirected)
-  A = (rand(N) <= p);
+% Existing ER generators for MATLAB aren't scalable for massive graphs because
+% they create a dense random matrix.
+% This implementation scales by initializing one row at a time.
+function [A] = erdos_generator(N, p, isDirected, seed)
+  if (nargin == 4)
+    rand('seed',seed);
+  end
+
+  A = sparse(N,N);
+  
+  for i=1:N
+    rand('seed',seed+i);
+    vec = rand(N,1);
+    vec = vec < p;
+    vec = sparse(vec);
+    A(:,i) = vec;
+  end
+
   A = A(~eye(N)); %remove self-edges
   if (nargin == 3)
     if (~isDirected)
       T = tril(A);
       A = T + T';
     end
-  else %assume directed
+  else %assume undirected
     T = tril(A);
     A = T + T';
   end
