40ed49fa3d61474e037bf721f71dc27d252e2fb9
Casey John Battaglino <cbattaglino3@ion.cc.gatech.edu>
2013-10-09 20:26:04 -0400

    growerdos

--- mfiles/grow_erdos.m
+++ mfiles/grow_erdos.m
@@ -1,14 +1,14 @@
 % This is a benchmarking experiment where we grow ER(N,p) from t=0 -> infty
 % We generate a directed graph with no self-edges
-function [times edges] = grow_erdos(N,max_nnz,seed)
+function [edges times zs] = grow_erdos(N,max_nnz,seed)
   
-  iterations = 100; %number of iterations between p_min and p_max
-  trials = 10; %number of benchmark trials to compute average
+  iterations = 20; %number of iterations between p_min and p_max
+  trials = 2; %number of benchmark trials to compute average
   lbound = 100; %default lowerbound on nnz
   bound = 1*10^6; %default upperbound on nnz
   times = []; %vector of benchmark results
   edges = []; %vector of nnz's
-
+  zs = []; %average degrees
   if (nargin > 1)
     if(nargin > 2)
       rand('seed',seed);
@@ -18,10 +18,10 @@ function [times edges] = grow_erdos(N,max_nnz,seed)
 
   p_min = lbound / (N^2 - N); %prob that will gen 'lbound' edges
   p_max = bound / (N^2 - N); %prob that will gen 'bound' edges
-  
+  grain = (p_max-p_min)/iterations; 
   x = ones(N,1);
 
-  for prob = p_min:1:p_max
+  for prob = p_min:grain:p_max
     ER = erdos_generator(N,prob);
     t = 0;
 
@@ -33,7 +33,8 @@ function [times edges] = grow_erdos(N,max_nnz,seed)
 
     t = t / trials;
     times = [times t];
-    edges = [edges nnz(ER)];
+    edges = [edges nnz(ER)/(10^6)]; %megabytes of nnz
+    zs = [zs prob*(N-1)];
   end
 
 end
