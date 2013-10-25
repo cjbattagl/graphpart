863ece6cadff761f0d5d71d687fd8f498a3d5d80
R. P <robert.s.pienta@gmail.com>
2013-10-25 01:01:08 -0400

    update to fennel

--- mfiles/heuristics/fennel.m
+++ mfiles/heuristics/fennel.m
@@ -3,47 +3,48 @@
 % I will fix it X_X
 
 function [B] = fennel (A, gamma)
-  vorder = bfsorder(A);
+  vorder = randperm(size(A,1));
 
   m = size(A,1);
   e = nnz(A);
 
-  alpha = e*((2^(gamma-1))/m^gamma);
+%   alpha = e*((2^(gamma-1))/m^gamma);
+  % experimental section alpha
+  alpha = sqrt(2)*e/m^1.5;
 
   p1 = [];
   p2 = [];
-  gp1 = 0; % maintain e(S1,V\S1)
-  gp2 = 0; % maintain e(S2,V\S2)
-
+  
   for v=vorder
     idxs = find(A(v,:));  %get N(v)
-    [tf, loc] = ismember(idxs, p1); %set loc to idxs of intersect(N(v), S1)
+
+    [~, loc] = ismember(idxs, p1); %set loc to idxs of intersect(N(v), S1)
     loc = find(loc); %length(loc) is now number of edges between v, S1
-    p1newedges = length(idxs) - length(loc); %p1newedges is number of edges between v, V \ S1 = e(v, V \ S1)
-    tempgp1 = gp1 + p1newedges; %tempgp1 is e(S1 U {v}, V \ (S1 U {v}))
-    g1 = tempgp1 - c(length(p1), alpha, gamma); %h(S1 U {v})
+    p1newedges = -length(idxs) + length(loc); %p1newedges is number of edges between v, V \ S1 = e(v, V \ S1)
+    clear loc
     
-    [tf, loc] = ismember(idxs, p2);
+    [~, loc] = ismember(idxs, p2);
     loc = find(loc);
-    p2newedges = length(idxs) - length(loc);
-    tempgp2 = gp2 + p2newedges;
-    g2 = tempgp2 - c(length(p2), alpha, gamma); %h(S2 U {v})
+    p2newedges = -length(idxs) + length(loc);
 
     % if h(S2) + h(S1 U {v}) > h(S1) + h(S2 U {v})
-    if (((gp2 - c(length(p2),alpha,gamma))+ g1) > ((gp1- c(length(p1),alpha,gamma)) + g2))
+    if (p1newedges - dc(length(p1),alpha, gamma)) >= (p2newedges - dc(length(p2),alpha,gamma))
       p1 = [p1 v];
-      gp1 = g1;
     else
       p2 = [p2 v];
-      gp2 = g2;
     end
   end
-  %disp(size(p1))
-  %disp(size(p2))
-  B = A([p1 p2], [p1 p2]); 
+%   disp(size(p1))
+%   disp(size(p2))
+  
+  
+  B = A([p1 p2],[p1 p2]); 
 end
 
 function [y] = c(x, alpha, gamma)
-  y = alpha * x^gamma;
+  y = alpha * x^(gamma);
 end
 
+function [dy] = dc(x, alpha, gamma)
+  dy = c(x+1, alpha, gamma) - c(x, alpha, gamma);
+end
