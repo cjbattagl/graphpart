8f3370b625d4b321e31da3bd9a800e539ff5cbf1
admin <admin@cc.local>
2013-10-30 11:12:30 -0400

    bebop util include

--- cfiles/fennel/fennel.cc
+++ cfiles/fennel/fennel.cc
@@ -1,4 +1,12 @@
 #include "fennel.h"
+#include <bebop/util/config.h>
+#include <bebop/smc/sparse_matrix.h>
+#include <bebop/smc/bcoo_matrix.h>
+#include <bebop/smc/bcsr_matrix.h>
+#include <bebop/smc/coo_matrix.h>
+#include <bebop/smc/csc_matrix.h>
+#include <bebop/smc/csr_matrix.h>
+#include <bebop/smc/jad_matrix.h>
 
 int main( void ) {
   return 0;
