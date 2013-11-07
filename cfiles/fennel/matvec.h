193d1293bd0e92968b4f2e926a43f94e0525e38a
admin <admin@cc.local>
2013-11-07 07:38:01 -0500

    rm matvec code

--- cfiles/fennel/matvec.h
+++
@@ -1,8 +0,0 @@
-#ifndef INC_MATVEC_H
-#define INC_MATVEC_H
-
-#include "csr.h"
-
-void csr_matvec__sequential (double* y, const csr_t* A, const double* x);
-int matvec (const csr_t* Adata, const double* b, double* x, int n, double* rhist, int maxiter);
-#endif
