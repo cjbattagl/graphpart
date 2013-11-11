dcd5f5e4af66a123d56337ac31a1aa1e29e64278
Casey Battaglino <cjbattagl@gmail.com>
2013-11-11 15:03:08 -0500

    sew stepid

--- cfiles/fennel/fennel.c
+++ cfiles/fennel/fennel.c
@@ -44,6 +44,8 @@
 #include <bebop/smc/interface.h>
 #include <bebop/smc/sparse_matrix_ops.h>
 
+#include <bebop/util/merge_sort.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -169,12 +171,27 @@ int main (int argc, char *argv[]) {
   
   if (got_arg_p (arglist, 'v')) { printf ("done, in %g seconds\n", seconds); }
 
+  fprintf (stdout, "Current format: ");
+  switch(A->format) {
+    case CSR:
+      fprintf (stdout, "CSR\n"); break;
+    case COO:
+      fprintf (stdout, "COO\n"); break;
+    case CSC:
+      fprintf (stdout, "CSC\n"); break;
+    default:
+      fprintf (stdout, "ERROR\n"); 
+  }
 
-  fprintf (stdout, "\n===== Converting to CSC =====\n");
-  sp_convert(A, "CSC");
-  assert(A->format == CSC);
+  fprintf (stdout, "\n===== Converting to CSR =====\n");
+  if(A->format == COO) {
+    coo_to_csr_convert(A); 
+  }
+  else {
+    sp_convert(A, "CSR");
+  }
 
-  struct csc_matrix_t* repr = A->repr;
+  struct csr_matrix_t* repr = A->repr;
   
   fprintf (stdout, "m = %d, n = %d, nnz = %d, density = %f, val type = ",repr->m,repr->n,repr->nnz,
     (float)repr->nnz/(float)(repr->m * repr->n),repr->value_type);
@@ -189,29 +206,29 @@ int main (int argc, char *argv[]) {
     default:
       fprintf (stdout, "ERROR\n"); 
   }
+ 
   
   // ********** Run FENNEL ***************************************
   fprintf (stdout, "\n===== Running fennel =====\n");
   run_fennel(repr, 2, 1.2); //todo: nparts, gamma as inputs
   // *************************************************************
-  //errcode = save_sparse_matrix ("out.mtx", A, MATRIX_MARKET);
+  errcode = save_sparse_matrix ("out.mtx", A, MATRIX_MARKET);
   destroy_sparse_matrix (A);
   return 0;
 }
 
-
-static int run_fennel(const struct csc_matrix_t* A, int nparts, float gamma) {
+static int run_fennel(const struct csr_matrix_t* A, int nparts, float gamma) {
   int m, n, nnz;
   void* values;
-  int* colptr;
-  int* rowidx;
+  int* colidx;
+  int* rowptr;
   enum symmetry_type_t symmetry_type;
   enum symmetric_storage_location_t symmetric_storage_location;
   enum value_type_t value_type;
   
   double seconds = 0.0;
 
-  unpack_csc_matrix (A, &m, &n, &nnz, &values, &rowidx, &colptr, &symmetry_type,
+  unpack_csr_matrix (A, &m, &n, &nnz, &values, &colidx, &rowptr, &symmetry_type,
 		   &symmetric_storage_location, &value_type);
 
   // Set alpha
@@ -248,8 +265,8 @@ static int run_fennel(const struct csc_matrix_t* A, int nparts, float gamma) {
   // Iterate over vorder
   fprintf (stdout, "----> Begin outer loop... \n");
   int vert, k, s, node = 0;
-  int *col;
-  int nnz_col = 0;
+  int *row;
+  int nnz_row = 0;
   int *partscore = (int*)malloc(nparts * sizeof(int));
   int *partsize = (int*)malloc(nparts * sizeof(int));
   
@@ -269,23 +286,23 @@ static int run_fennel(const struct csc_matrix_t* A, int nparts, float gamma) {
   for (int i = 0; i < n; i++) {
     for (s = 0; s < nparts; s++) { partscore[s]=0; }
     vert = vorder[i];
-    col = &colptr[vert];
-    nnz_col = *(col+1) - *col;
-    //fprintf (stdout, " %d ",nnz_col);
+    row = &rowptr[vert];
+    nnz_row = *(row+1) - *row;
+    //fprintf (stdout, " %d ",nnz_row);
 
-   if(nnz_col != 0) {
+   if(nnz_row != 0) {
       // generate partition scores for each partition
-      for (k = *col; k < ((*col)+nnz_col); k++) {
-        node = rowidx[k];
+      for (k = *row; k < ((*row)+nnz_row); k++) {
+        node = colidx[k];
         for (s = 0; s < nparts; s++) { if (parts[s][node] == 1) { partscore[s]++; /*break;*/ }}
       }
     
       // choose optimal partition (initializing first)
-      best_score = (partscore[0]-nnz_col) - ((alpha*pow(partsize[0]+1,gamma)) - (alpha*pow(partsize[0],gamma)));
+      best_score = (partscore[0]-nnz_row) - ((alpha*pow(partsize[0]+1,gamma)) - (alpha*pow(partsize[0],gamma)));
       best_part = 0;
     
       for (s = 1; s < nparts; s++) {
-        curr_score = (partscore[s]-nnz_col) - ((alpha*pow(partsize[s]+1,gamma)) - (alpha*pow(partsize[s],gamma)));
+        curr_score = (partscore[s]-nnz_row) - ((alpha*pow(partsize[s]+1,gamma)) - (alpha*pow(partsize[s],gamma)));
         if (curr_score > best_score) { best_score = curr_score; best_part = s; }
       }
     
@@ -321,8 +338,8 @@ static int run_fennel(const struct csc_matrix_t* A, int nparts, float gamma) {
   
   for (int i = 0; i < n; i++) {
     vert = i;
-    col = &colptr[vert];
-    nnz_col = *(col+1) - *(col); //nnz in row
+    row = &rowptr[vert];
+    nnz_row = *(row+1) - *(row); //nnz in row
     v_part = -1;
     
     // find v's partition
@@ -338,8 +355,8 @@ static int run_fennel(const struct csc_matrix_t* A, int nparts, float gamma) {
     }
     
     // count edges to other partitions
-    for (k = *col; k < ((*col)+nnz_col); k++) {
-      if (parts[v_part][rowidx[k]] != 1) { cutedges++; }
+    for (k = *row; k < ((*row)+nnz_row); k++) {
+      if (parts[v_part][colidx[k]] != 1) { cutedges++; }
     }
   }
   
@@ -351,6 +368,194 @@ static int run_fennel(const struct csc_matrix_t* A, int nparts, float gamma) {
   fprintf (stdout, "----> Overassigned vertices (error): %d\n", redparts);
   fprintf (stdout, "----> Empty vertices: %d\n", emptyverts);
 }
+
+struct csr_matrix_t* mat_to_csr (struct coo_matrix_t* A) {
+  int m   = A->m;
+  int n   = A->n;
+  int nnz = A->nnz;
+  enum symmetry_type_t symmetry_type = A->symmetry_type;
+  enum symmetric_storage_location_t symmetric_storage_location = A->symmetric_storage_location;
+  enum value_type_t value_type = A->value_type;
+  int    *rowptr = NULL;
+  int    *colidx = NULL;
+  void   *__values = NULL;
+  void   *__coord_array = NULL;
+  int i, j, currow = 0;
+  struct csr_matrix_t* B = bebop_calloc (1, sizeof (struct csr_matrix_t));
+  int index_base = A->index_base;
+
+  /* bebop_set_debug_level (2); */
+
+  fprintf (stdout, "=== coo_to_csr ===\n");
+  fprintf (stdout, "index_base=%d\n",index_base);
+
+  fprintf (stdout, "\tm = %d, n = %d, nnz = %d, value_type = %d\n", m, n, nnz, value_type);
+
+  if (A->value_type == REAL)
+    __values = bebop_calloc (nnz, sizeof (double));
+  else if (A->value_type == PATTERN)
+    __values = NULL;
+  else {
+      fprintf (stdout, "*** coo_to_csr: input matrix in COO format has"
+	       " invalid value type %d ***\n", A->value_type);
+      bebop_exit (EXIT_FAILURE);
+  }
+
+  rowptr = bebop_calloc ((m+1), sizeof (int));
+  colidx = bebop_calloc (nnz,   sizeof (int));
+
+  if (nnz == 0) {
+      /* calloc fills in rowptr with zeros, which is all 
+         we need if the matrix is empty */
+      fprintf (stdout, "\tMatrix is empty, done\n");
+      init_csr_matrix (B, m, n, nnz, __values, colidx, rowptr, UNSYMMETRIC, 
+		       UPPER_TRIANGLE, value_type, LIBRARY_DEALLOCATES,
+		       &free, NO_COPY);
+      fprintf (stdout, "=== Done with coo_to_csr_matrix ===\n");
+      return B; 
+    }
+
+  /* Intermediate conversion to coordinate array, so we can sort the entries */
+  fprintf (stdout, "\tIntermediate conversion to coordinate array format\n");
+  coo_matrix_to_coord_elem_array (&__coord_array, &nnz, A);
+
+  fprintf (stdout, "\tSorting elements of coordinate array\n");
+  sort_coo (__coord_array, nnz, value_type);
+
+  if (value_type == REAL) {
+      struct coord_array_t { int r; int c; double val; };
+      struct coord_array_t* coord_array = (struct coord_array_t*) __coord_array;
+      double* values = (double*) __values;
+
+      fprintf (stdout, "\tReal value specialization\n");
+
+      /*
+       * Having sorted the elements of the coordinate array, first by initial 
+       * row, then within each row by initial column, now the first row with 
+       * nonzeros is coord_array[0].r.
+       */
+      fprintf (stdout, "\tInitializing start of rowptr\n");
+      currow = coord_array[0].r - index_base;
+      for (i = 0; i <= currow; i++)
+	{
+	  /* 
+	   * Until we get to first row with an entry in it, all the rowptrs 
+	   * before then are zero.  The rowptr for that first column is also 
+	   * zero. 
+	   */
+	  rowptr[i] = 0;  
+	}
+      /* For each coord_array entry i, add it to the CSR matrix */
+      fprintf (stdout, "\tAdding entries to CSR matrix\n");
+      for (i = 0; i < nnz; i++)
+	{
+	  //fprintf (stdout, "\t\ti = %d of %d\n", i, nnz);
+	  if (coord_array[i].r - index_base > currow)
+	    {
+	      /* 
+	       * We may jump more than one row at a time, so set the rowptr 
+	       * entries for the empty rows in between.
+	       */
+	      for (j = currow+1; j <= coord_array[i].r - index_base; j++) 
+		{
+		  if (j - index_base < 0 || j - index_base > m)
+		    {
+		      bebop_log (0, "*** At entry %d of %d, "
+				"j = %d is out of the valid range "
+				"[%d,%d] ***\n", 
+				i, nnz, j - index_base, 0, m);
+		      bebop_free (values);
+		      bebop_free (rowptr);
+		      bebop_free (colidx);
+		      bebop_free (B);
+		      return NULL;
+		    }
+		  rowptr[j] = i;
+		}
+
+	      currow = coord_array[i].r - index_base;
+	    }
+
+	  values[i] = coord_array[i].val;   
+	  colidx[i] = coord_array[i].c - index_base;
+	}
+
+      /* Set the last entries in rowptr appropriately */
+      for (j = currow+1; j <= m; j++)
+	rowptr[j] = nnz;
+
+      init_csr_matrix (B, m, n, nnz, __values, colidx, rowptr, symmetry_type, 
+		       symmetric_storage_location, value_type, 
+		       LIBRARY_DEALLOCATES, &free, NO_COPY);
+    }
+  else if (value_type == PATTERN)
+    {
+      struct coord_array_t { int r; int c; };
+      struct coord_array_t* coord_array = (struct coord_array_t*) __coord_array;
+
+      /*
+       * Having sorted the elements of the coordinate array, first by initial 
+       * row, then within each row by initial column, now the first row with nonzeros is
+       * coord_array[0].r.
+       */
+      currow = coord_array[0].r - index_base;
+      for (i = 0; i <= currow; i++) {
+	  /* 
+	   * Until we get to first row with an entry in it, all the rowptrs
+	   * before then are zero.  The rowptr for that first row is also zero. 
+	   */
+	  rowptr[i] = 0;  
+	  fprintf (stdout, "\tset rowptr[%d] to %d\n", i, 0);
+	  }
+      /* For each coord_array entry i, add it to the CSR matrix */
+      for (i = 0; i < nnz; i++) {
+      fprintf (stdout, "\ti = %d, c.r[i] = %d, c.c[i] = %d\n", i, coord_array[i].r, coord_array[i].c);
+	  if (coord_array[i].c - index_base > currow) {
+	      /* 
+	       * We may jump more than one row at a time, so set the rowptr 
+	       * entries for the empty rows in between.
+	       */
+	      for (j = currow+1; j <= coord_array[i].r - index_base; j++) {
+		    rowptr[j] = i;
+		    fprintf (stdout, "\tset rowptr[%d] to %d\n", j, i);
+		  }
+	      currow = coord_array[i].r - index_base;
+	    }
+		fprintf (stdout, "\tset colidx[%d] to %d\n", i, coord_array[i].c - index_base);
+        colidx[i] = coord_array[i].c - index_base;
+	  }
+
+      /* Set the last entries in rowptr appropriately */
+      for (j = currow+1; j <= m; j++) { rowptr[j] = nnz; 
+      		    fprintf (stdout, "\tset rowptr[%d] to %d\n", j, nnz);
+}
+
+      init_csr_matrix (B, m, n, nnz, NULL, colidx, rowptr, symmetry_type, 
+		       symmetric_storage_location, value_type,
+		       LIBRARY_DEALLOCATES, &free, NO_COPY);
+    }
+
+  bebop_free (__coord_array);
+  fprintf (stdout, "=== Done with coo_to_csr ===\n");
+  return B;
+}
+
+void sort_coo (void* coord_array, const int length, enum value_type_t value_type) { 
+  fprintf (stdout, "=== sort_coord_elem_array_for_csr_conversion ===\n");
+
+  if (value_type == REAL) {
+    struct coord_elem_t { int r; int c; double val; };
+    merge_sort (coord_array, length, sizeof (struct coord_elem_t), coord_elem_by_col_real);
+    merge_sort (coord_array, length, sizeof (struct coord_elem_t), coord_elem_by_row_real);
+  }
+  else if (value_type == PATTERN) {
+    struct coord_elem_t { int r; int c; };
+    merge_sort (coord_array, length, sizeof (struct coord_elem_t), coord_elem_by_col_pattern);
+    merge_sort (coord_array, length, sizeof (struct coord_elem_t), coord_elem_by_row_pattern);
+  }
+  fprintf (stdout, "=== Done with sort_coord_elem_array_for_csr_conversion ===\n");
+}
+
 
 /**
  * Perform the matrix validation operation specified by the "-a" command-line option.
@@ -418,5 +623,97 @@ do_validate_matrix (int argc, char *argv[], struct arginfo* arglist)
   else { printf ("\n### Invalid sparse matrix! ###\n\n"); }
 
   destroy_sparse_matrix (A);
+  return 0;
+}
+
+static int coo_to_csr_convert(struct sparse_matrix_t* A) {
+  assert(A->format == COO);
+  struct csr_matrix_t* B = mat_to_csr (A->repr);
+  if (B == NULL) { return -1; }
+  destroy_coo_matrix (A->repr);
+  A->repr = B; A->format = CSR;
+  return 0;
+}
+
+int coord_elem_by_row_real (const void* a, const void* b) {
+  struct coord_elem_t
+  {
+    int r;
+    int c;
+    double val;
+  };
+
+  const struct coord_elem_t* x = ((struct coord_elem_t*) a);
+  const struct coord_elem_t* y = ((struct coord_elem_t*) b);
+
+  if (x->r < y->r) 
+    return -1;
+  else if (x->r > y->r)
+    return +1;
+
+  /* else */
+  return 0;
+}
+
+int coord_elem_by_row_pattern (const void* a, const void* b) {
+  struct coord_elem_t
+  {
+    int r;
+    int c;
+  };
+
+  const struct coord_elem_t* x = ((struct coord_elem_t*) a);
+  const struct coord_elem_t* y = ((struct coord_elem_t*) b);
+
+  if (x->r < y->r) 
+    return -1;
+  else if (x->r > y->r)
+    return +1;
+
+  /* else */
+  return 0;
+}
+
+
+/**
+ * Compares two coordinate-array elements by their column indices, and 
+ * returns values like strcmp does.  Comparison function for sorting.
+ */
+int coord_elem_by_col_real (const void* a, const void* b) {
+  struct coord_elem_t
+  {
+    int r;
+    int c;
+    double val;
+  };
+
+  const struct coord_elem_t* x = ((struct coord_elem_t*) a);
+  const struct coord_elem_t* y = ((struct coord_elem_t*) b);
+
+  if (x->c < y->c) 
+    return -1;
+  else if (x->c > y->c)
+    return +1;
+
+  /* else */
+  return 0;
+}
+
+int coord_elem_by_col_pattern (const void* a, const void* b) {
+  struct coord_elem_t
+  {
+    int r;
+    int c;
+  };
+
+  const struct coord_elem_t* x = ((struct coord_elem_t*) a);
+  const struct coord_elem_t* y = ((struct coord_elem_t*) b);
+
+  if (x->c < y->c) 
+    return -1;
+  else if (x->c > y->c)
+    return +1;
+
+  /* else */
   return 0;
 }
--- cfiles/fennel/fennel.h
+++ cfiles/fennel/fennel.h
@@ -1,4 +1,15 @@
 static void usage (FILE* out, const struct arginfo* arglist, const struct arginfo* ext_args);
 static int do_validate_matrix (int argc, char *argv[], struct arginfo* arglist);
 
-static int run_fennel (const struct csc_matrix_t* A, int nparts, float gamma);
+//util
+struct csr_matrix_t* mat_to_csr (struct coo_matrix_t* A);
+void sort_coo (void* coord_array, const int length, enum value_type_t value_type);
+int coord_elem_by_row_real (const void* a, const void* b);
+int coord_elem_by_row_pattern (const void* a, const void* b);
+int coord_elem_by_col_real (const void* a, const void* b);
+int coord_elem_by_col_pattern (const void* a, const void* b);
+static int coo_to_csr_convert(struct sparse_matrix_t* A);
+
+
+
+static int run_fennel (const struct csr_matrix_t* A, int nparts, float gamma);
