58f1331760fa83bfd36434b5ae69e7fbe81d8786
Casey Battaglino <cjbattagl@gmail.com>
2013-11-21 12:10:28 -0500

    more stats in test.m

--- cfiles/fennel/test.m
+++ cfiles/fennel/test.m
@@ -1,3 +1,7 @@
+%How to use:
+% ./fennel -v -e 'ca-AstroPh.mtx' 'MM'
+% [B A cut]=test('ca-AstroPh.mat');
+
 function [B A cut] = test(fname)
   a=load('parts.mat');
   A=load(fname);
@@ -17,13 +21,37 @@ function [B A cut] = test(fname)
     idxlist(i,:) = [lo hi];
     psizes(i) = length(t);
   end
-  assert(length(P)==N);
+
   B=A(P,P);
+
+  % Sanity checks
+  assert(length(unique(P))==length(P));
+  assert(length(P)==N);
   assert(nnz(A)==nnz(B));
+  
+  % Verify cut size
   cut=nnz(B);
   for i=1:nparts
     range = idxlist(i,1):idxlist(i,2);
-    cut = cut - nnz(B(range,range));
+    cut = cut - full(nnz(B(range,range)));
   end
 
+  %%%%%% Other statistics:
+
+  % Compute densities of partitions
+  disp('Density of subgraphs:');
+  for i=1:nparts
+    range = idxlist(i,1):idxlist(i,2);
+    dens = full(nnz(B(range,range))) / length(range)^2;
+    fprintf('[%d]:\t%1.2f %%\n',i,100*dens);
+  end
+  disp('');
+  % Compute avg degree of partitions
+  disp('Average degrees:');
+  for i=1:nparts
+    range = idxlist(i,1):idxlist(i,2);
+    deg = (nnz(B(range,:))/length(range));
+    fprintf('[%d]:\t%d\n',i,deg);
+  end
+  disp('');
 end
