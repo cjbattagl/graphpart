cc4c0c520adf4807b6b79c1e2752031c878a31d4
cjbattagl <cjbattagl@gmail.com>
2014-01-31 01:37:33 -0500

    graph metr 1

--- pyfiles/read.py
+++ pyfiles/read.py
@@ -1,5 +1,6 @@
 import scipy.io as sio
 import numpy as np
+import networkx as nx
 from bokeh.plotting import *
 from math import floor
 
@@ -8,10 +9,7 @@ try:
 except:
     raise
 
-import networkx as nx
-
 output_file("read.html")
-
 name = 'test.mtx'
 info = sio.mminfo(name)
 n = info[0]
@@ -22,40 +20,36 @@ print("size:      {} x {}".format(n,n))
 print("nonzeros:  {} (density: {})".format(nnz, round(float(nnz) / (n * n),6)))
 print("nnz type:  {} {}".format(info[5],info[4]))
 assert(n==info[1])
-
-print("reading matrix ..."),
+print("reading matrix ..."), 
 A = sio.mmread(name)
-print("done\nconverting matrix to csr ..."),
+print("done\nconverting matrix to csr ..."), 
 A = A.tocsr()
 print("done")
 
 G = nx.to_networkx_graph(A)
 step = 0
-numsamples = 100
+numsamples = n
 p = 10
-
 procbin = float(n)/p
 sampbin = math.floor(float(n)/numsamples)
-
 front = np.zeros(p)
 history = np.zeros((p, numsamples))
 
-
-for node in nx.bfs_edges(G, 1):
+for node in nx.minimum_spanning_edges(G):
   step += 1
   targnode = floor(node[1] / procbin)
   front[targnode] += 1
-  if (step % numsamples == 0) :
+  if (step % sampbin == 0) :
     for proc in range(p):
       history[proc, step/sampbin] = front[proc]
 
-x = np.linspace(0, 100, 100) 
+print(front)
 
+x = np.linspace(0, 100, numsamples) 
 hold()
 
 for proc in range(p):
-  line(x, history[proc,:], color="#0000FF", x_axis_type = "datetime", 
-    tools="pan,zoom,resize", width=1200,height=300, 
+  line(x, history[proc,:], color="#0000FF", 
     title = 'Load')
  
 xaxis()[0].axis_label = "Chunk"
