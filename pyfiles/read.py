dfb04693870eccd8cbbba68b3b9c833b75e41cb3
cjbattagl <cjbattagl@gmail.com>
2014-02-05 01:17:17 -0500

    made basic bfs tree traversal

--- pyfiles/read.py
+++ pyfiles/read.py
@@ -1,6 +1,5 @@
 #goal: communication model for 1D and 2D BFS,
 #and finally a hybrid 1D/2D
-
 import scipy.io as sio
 import numpy as np
 import networkx as nx
@@ -11,75 +10,70 @@ try:
     import matplotlib.pyplot as plt
 except:
     raise
-    
-def my_bfs_edges(G,source):
-    visited = set([source])
-    stack = [(source,iter(G[source]))]
-    while stack:
-        parent,children = stack[0]
-        try:
-            child = next(children)
-            if child not in visited:
-                yield parent,child
-                visited.add(child)
-                stack.append((child,iter(G[child])))
-        except StopIteration:
-            stack.pop(0)
 
 output_file("read.html")
-name = 'ca-AstroPh.mtx'
-info = sio.mminfo(name)
-n = info[0]
-nnz = info[2]
+mat_name = 'ca-HepTh.mtx'
+mat_info = sio.mminfo(mat_name)
+mat_n = mat_info[0]
+mat_nnz = mat_info[2]
 
-print("matrix: {}".format(name))
-print("size: {} x {}".format(n,n))
-print("nonzeros: {} (density: {})".format(nnz, round(float(nnz) / (n * n),6)))
-print("nnz type: {} {}".format(info[5],info[4]))
-assert(n==info[1])
+print("matrix: {}".format(mat_name))
+print("size: {} x {}".format(mat_n, mat_n))
+print("nonzeros: {} (density: {})".format(mat_nnz, round(float(mat_nnz) / (mat_n * mat_n),6)))
+print("nnz type: {} {}".format(mat_info[5], mat_info[4]))
+assert(mat_n == mat_info[1])
 print("reading matrix ..."),
-A = sio.mmread(name)
+A = sio.mmread(mat_name)
 print("done\nconverting matrix to csr ..."),
 A = A.tocsr()
 print("done")
 
 G = nx.to_networkx_graph(A)
+numsamples = mat_n  #number of samples to take
+numprocs = 4        #number of processors to simulate
+s = 400             #starting vertex
+procbin = float(mat_n)/numprocs
+sampbin = floor(float(mat_n)/numsamples)
+front = np.zeros(numprocs)
+history = np.zeros((numprocs, numsamples))
+
 step = 0
-numsamples = n
-p = 4
-s = 500
-procbin = float(n)/p
-sampbin = floor(float(n)/numsamples)
-front = np.zeros(p)
-history = np.zeros((p, numsamples))
 
 def share_same_proc(node1, node2):
-  return (floor(node1 / procbin) == (floor(node2 / procbin)
+  return (floor(node1 / procbin) == floor(node2 / procbin))
+  
+def do_shit():
+  for node in bfs_edges(G, s):
+    step += 1
+    targnode = floor(node[1] / procbin)
+    if (targnode != floor(node[0] / procbin)) :
+      front[targnode] += 1
+    if (step % sampbin == 0) :
+      for proc in range(numprocs):
+        history[proc, step/sampbin] = front[proc]
+        
+def do_1d(G, s):
+  iter = 0
+  numnodes = 0
+  T = nx.bfs_tree(G,s)
+  nextlevel = set()
+  succ = T.successors(s)
+  while (len(succ) > 0):
+    for node in succ:
+      nextlevel = nextlevel.union(T.successors(node))
+      numnodes += 1
+    succ = list(nextlevel)
+    nextlevel.clear()
+  print(numnodes)
 
-for node in my_bfs_edges(G, s):
-  step += 1
-  targnode = floor(node[1] / procbin)
-  if (targnode != floor(node[0] / procbin)) :
-    front[targnode] += 1
-  if (step % sampbin == 0) :
-    for proc in range(p):
-      history[proc, step/sampbin] = front[proc]
-      
-T = nx.bfs_tree(G,s)
-succ = T.successors(s)
-nextlevel = []
-while (len(succ) > 0):
-  for node in succ:
-    
-    nextlevel = nextlevel + T.successors(node)
-    
+do_1d(G,s)
 
-print(front)
+#print(front)
 
 x = np.linspace(0, numsamples, numsamples)
 hold()
 
-for proc in range(p):
+for proc in range(numprocs):
   line(x, history[proc,:], color="#0000FF",
     title = 'Load')
  
@@ -88,4 +82,4 @@ yaxis()[0].axis_label = "Load"
 #show()
 
 print("pct. communicated edges: %{}".format(round(100*np.sum(front)/step),5))
-print("exp. communicated edges: %{}".format(round(100*((float(p)-1)/p),5)))
+print("exp. communicated edges: %{}".format(round(100*((float(numprocs)-1)/numprocs),5)))
