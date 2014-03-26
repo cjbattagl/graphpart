f95de033e1e33ce869718777215ee2bfa5e77dca
Casey Battaglino <cjbattagl@gmail.com>
2014-03-26 17:54:21 -0400

    mods

--- cfiles/external/graph500-mpi/bfs_lohi.c
+++ cfiles/external/graph500-mpi/bfs_lohi.c
@@ -3,7 +3,7 @@
 /* Use, modification and distribution is subject to the Boost Software     */
 /* License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at */
 /* http://www.boost.org/LICENSE_1_0.txt)                                   */
-/*                                                                         */
+/*  Adapted from code by:                                                  */
 /*  Authors: Jeremiah Willcock                                             */
 /*           Andrew Lumsdaine                                              */
 
@@ -29,7 +29,46 @@ static MPI_Request* g_outgoing_reqs;
 static int* g_outgoing_reqs_active;
 static int64_t* g_recvbuf;
 
+  /* Predefined entities you can use in your BFS (from common.h and oned_csr.h):
+   *   + rank: global variable containing MPI rank
+   *   + size: global variable containing MPI size
+   *   + DIV_SIZE: single-parameter macro that divides by size (using a shift
+   *     when properly set up)
+   *   + MOD_SIZE: single-parameter macro that reduces modulo size (using a
+   *     mask when properly set up)
+   *   + VERTEX_OWNER: single-parameter macro returning the owner of a global
+   *     vertex number
+   *   + VERTEX_LOCAL: single-parameter macro returning the local offset of a
+   *     global vertex number
+   *   + VERTEX_TO_GLOBAL: single-parameter macro converting a local vertex
+   *     offset to a global number
+   *   + g.nlocalverts: number of vertices stored on the local rank
+   *   + g.nglobalverts: total number of vertices in the graph
+   *   + g.nlocaledges: number of graph edges stored locally
+   *   + g.rowstarts, g.column: zero-based compressed sparse row data
+   *     structure for the local part of the graph
+   *
+   * All macros documented above evaluate their arguments exactly once.
+   *
+   * The graph is stored using a 1-D, cyclic distribution: all edges incident
+   * to vertex v are stored on rank (v % size) (aka VERTEX_OWNER(v)).  Edges
+   * that are not self-loops are stored twice, once for each endpoint;
+   * duplicates edges are kept.  The neighbors of vertex v can be obtained on
+   * rank VERTEX_OWNER(v); they are stored in elements
+   * {g.rowstarts[VERTEX_LOCAL(v)] ... g.rowstarts[VERTEX_LOCAL(v) + 1] - 1}
+   * (inclusive) of g.column.
+   *
+   * Upon exit, your BFS must have filled in:
+   *   + pred (an array of size g.nlocalverts):
+   *     - The predecessor of vertex v in the BFS tree should go into
+   *       pred[VERTEX_LOCAL(v)] on rank VERTEX_OWNER(v)
+   *     - The predecessor of root is root
+   *     - The predecessor of any unreachable vertex is -1
+   *
+   * The validator will check this for correctness. */
+
 void make_graph_data_structure(const tuple_graph* const tg) {
+  // Communicate and distributes in a 1D CSR format
   convert_graph_to_oned_csr(tg, &g);
   const size_t nlocalverts = g.nlocalverts;
   g_oldq = (int64_t*)xmalloc(nlocalverts * sizeof(int64_t));
